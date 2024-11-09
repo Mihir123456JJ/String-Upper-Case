d449df5ec84d1bc6da7194e554ce1a31f38860ff
Mihir123456JJ <kalothiamihir@gmail.com>
2024-11-09 18:23:44 +0530

    String Upper Case

---
+++ main.py
@@ -0,0 +1,15 @@
+class xyz():
+    def __init__(self):
+        self.strl= ""
+    def getstring(self):
+        self.strl=input("Enter String:")
+    def printstring(self):
+        print("Answer is:",self.strl.upper()) 
+strl=xyz()
+strl. getstring()
+strl.printstring()
+
+
+
+
+
